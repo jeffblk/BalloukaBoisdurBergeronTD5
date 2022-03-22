edac44ec542d7af541aa13cec7c5df155e99d27b
Arthur-be <hammertume0@gmail.com>
2022-03-22 15:34:13 +0100

    Create book.py

---
+++ book.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 22 15:22:21 2022
+
+@author: arthu
+"""
+
+class Order:
+    def __init__(self, quantity, price):
+        self.__quantity = quantity
+        self.__price = price
+        
+    def quantity(self):
+        return self.__quantity
+
+    def price(self):
+        return self.__price
+
+
+class Book:
+    def __init__(self, name):
+        self.__name = name  # The name of the ordered book
+        self.buy_orders = []  #buy orders
+        self.sell_orders = []  #sell orders
+
+    def name(self):  # Getter
+        return self.__name
+
+    # Inserting into the order book new orders
+    def insert_buy(self, quantity, price):
+        self.buy_orders.append(Order(quantity, price))
+
+    def insert_sell(self, quantity, price):
+        self.sell_orders.append(Order(quantity, price))
